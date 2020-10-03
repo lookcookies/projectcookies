cefb63f4d699b2d82e02c874d441cac5159a03af
lookcookies <61165202+lookcookies@users.noreply.github.com>
2020-10-03 10:53:05 +0100

    Add files via upload

---
+++ Story Time Editor.py
@@ -0,0 +1,82 @@
+# Story time!
+import time
+
+print("Let me tell you a story")
+time.sleep(2)
+
+print("Once upon a time, there was a street dweller")
+time.sleep(4)
+
+name = (input("Her name was "))
+time.sleep(1)
+
+print(name + ", although a street dweller, was fair in her ways")
+time.sleep(4)
+
+print(name + " lived in a corrupt kingdom")
+time.sleep(4)
+
+kingdom =(input("This kingdom was called "))
+time.sleep(1)
+
+print(name + " hid in the alleys of " + kingdom + ", only venturing into the hussling and bustling of the Bazaar when the guards could not detect her presence")
+time.sleep(9)
+
+hour =(input("The Bazaar was usually busy, but the most busiest time was "))
+time.sleep(1)
+
+print("Sleeking through the crowds with her face covered and her weapon close to her side, " + name + " worked her way towards the weapons craft stool")
+time.sleep(8)
+
+print("Why was she there? A recent scuffle in the food market saw her fighting off two guards, who accused a street child of stealing bread, chipping away at her most prized weapon")
+time.sleep(12)
+
+weapon =(input("This prized weapon she held so close was a "))
+time.sleep(1)
+
+print("Yet this was no ordinary " + weapon)
+time.sleep(3)
+
+handle =(input("It was a hand crafted " + weapon + " with a handle made of "))
+time.sleep(1)
+
+inlay =(input("and an inlay of "))
+time.sleep(1)
+
+date = (input("It was passed to her by her Father, killed outside the Palace walls for protecting an innocent on the night of the "))
+time.sleep(1)
+
+father =(input("His name was "))
+time.sleep(1)
+
+killer =(input("His killer, "))
+time.sleep(4)
+
+print(name + ", although fair in her ways, swore vengence on her killer, " + killer + ", for the death of her father, " + father)
+time.sleep(9)
+
+print("And she swore to get it with her beautiful " + weapon + " crafted with a body of " + handle + " and an inlay of " + inlay)
+time.sleep(8)
+
+print("Only, three years on")
+time.sleep(3)
+
+print(date)
+time.sleep(2)
+
+print("She had to wait for the exact time")
+time.sleep(3)
+
+print("For when " + killer + " would return again, for her perfect vengence")
+time.sleep(10)
+
+
+
+
+
+
+
+
+
+
+
