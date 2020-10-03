bcf6202316ad7a1b31df3bde01bf5122130259f0
lookcookies <61165202+lookcookies@users.noreply.github.com>
2020-10-03 11:02:31 +0100

    The Answer Is Three
    Created in Python 3.8 IDLE.

---
+++ The Answer Is Three.py
@@ -0,0 +1,6 @@
+# Enter any number into this program and follow the prompts
+# The output is always 3
+
+number = float(input("Enter a number: "))
+Input = ((((number * 2) + 6)/2)- number)
+print("Your number output is: ", Input)
